1ee475fdf95ad11b9ad29893baec18736786aa06
Mohan <n01620006@humber.ca>
2025-02-25 10:40:12 -0500

    updated files

--- src/main/java/com/example/n016200006_mohankumar_test3ims/controller/ProductController.java
+++ src/main/java/com/example/n016200006_mohankumar_test3ims/controller/ProductController.java
@@ -8,13 +8,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ProductController {
@@ -24,46 +21,34 @@ public class ProductController {
 
     @GetMapping("/")
     public String showIndexPage(Model model) {
-        // Provide a fresh Student object for registration
-        model.addAttribute("Product", new Product());
-        return "index"; // index.html
+    	model.addAttribute("listProducts", productService.getAllProducts());
+        return "index"; // Assumes index.html exists in the template directory
     }
     
-    
-    @GetMapping("/products")
-    public List<Product> getAllProducts() {
-		return productService.getAllProducts();
-	}
-    
-    @GetMapping("/products/id")
-    public Product getProductById(@RequestBody Long id) {
-    	return productService.getProductById(id);
+    @GetMapping("/showNewProductForm")
+    public String showNewProductForm(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "new_product";
     }
     
-    @PostMapping("/products")
-    public void addProduct(@RequestBody Product product) {
-    Product existingProduct = productService.getProductById(product.getProductId());
-    if(existingProduct != null) {
-    				productService.updateProduct(product);
-				} else {
-					productService.addProduct(product);
-				}
+    @PostMapping("/saveProduct")
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        productService.addProduct(product);
+        return "redirect:/";
     }
     
-    @PutMapping("/products")
-    public void updateProduct(@RequestBody Product product) {
-    			productService.updateProduct(product);
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "update_product";
     }
     
-    @DeleteMapping("/products")
-    public void deleteProduct(@RequestBody Long id) {
-				productService.deleteProduct(id);
-	}
-    
-    @PutMapping("/products/stock")
-	public void updateStock(@RequestBody Product product) {
-				productService.updateStock(product);
-	}
-    
-    
+    @GetMapping("/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable(value = "id") long id) {
+        this.productService.deleteProduct(id);
+        return "redirect:/";
+    }
+
 }
